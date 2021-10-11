2052abbeddf46ebfb6a6d3c5b11d4a542aeca534
Solibex <zsolibex@gmail.com>
2021-10-12 02:19:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-	print("Hello world")
-	
+import pygame
+import sys
+
+pygame.init()
+
+
+running = True
+
+while running:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.exit()
+            sys.exit(0)
+print("Hello world")
